e7022952db061c25c84c6c4a5e38294901630d81
Davit Dzneladze <dzneladze0802@gmail.com>
2024-11-30 11:57:05 +0100

    Enabled cors

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
-    methods: '*',
-    allowedHeaders: '*',
+    origin: ['http://localhost:5173'], // Allow only specific domains
+    methods: 'GET,POST,PUT,DELETE', // Allow only specific HTTP methods
+    allowedHeaders: 'Content-Type,Authorization', // All
   });
 
   await app.listen(3000);
